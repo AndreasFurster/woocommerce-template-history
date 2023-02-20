4a92da01b9d3a720d62904f2d807087520b2c477
Andreas Furster <andreas.furster@hotmail.com>
2023-02-20 15:38:08 +0100

    Update download script

--- download.py
+++ download.py
@@ -11,7 +11,7 @@ template_version_regex = re.compile(r"\* @version\s+(.*)")
 
 # Clone the Git repository
 print('Cloning repository')
-Repo.clone_from(repo_url, local_dir)
+# Repo.clone_from(repo_url, local_dir)
 
 # Open the cloned repository
 repo = Repo(local_dir)
@@ -25,21 +25,29 @@ for tag in repo.tags:
     tag_name = tag.name.replace('v', '')
     major_version = int(tag_name.split('.')[0])
 
-    if major_version is 1:
+    if major_version < 5:
       continue
 
     template_location = 'plugins/woocommerce/templates/' if major_version >= 6 else 'templates/'
 
     # Checkout the tag to get the files
-    print('Downloading files from tag {}'.format(tag_name))
-    repo.git.checkout(tag)
+    try:  
+      print('Downloading files from tag {}'.format(tag_name))
+      repo.git.checkout(tag)
+    except:
+      print('Failed to checkout tag {}. Skipping...'.format(tag_name))
+      continue
 
     template_tag_search = '* @version {}'.format(tag_name)
 
     # Copy the files from the repository to the tag folder
     for file in repo.tree().traverse():
         if file.type == 'blob' and file.path.startswith(template_location):
-            file_content = repo.git.show('{}:{}'.format(tag_name, file.path))
+            try:
+              file_content = repo.git.show('{}:{}'.format(tag_name, file.path))
+            except:
+              print('Failed to get file content for file {}. Skipping...'.format(file.path))
+              continue
             
             # Skip if version is not current tag. This means the file is not updated in this version. 
             matches = template_version_regex.search(file_content)
@@ -63,4 +71,4 @@ for tag in repo.tags:
             
             with open(file_path, 'wb') as f:
                 print('Writing file {}'.format(file_path))
-                f.write(repo.git.show('{}:{}'.format(tag_name, file.path)).encode())
+                f.write(file_content.encode())
